d5d701aa3266563afe6650593caed24e52824422
Eden <edenginting90@yahoo.com>
2022-07-06 11:14:50 +0800

    CharacterLists components updated

--- components/CharactersLists.tsx
+++ components/CharactersLists.tsx
@@ -1,23 +1,9 @@
-import Image from "next/image";
 import classes from "./CharactersLists.module.css";
-import Link from "next/link";
 import Paginate from "./ui/Paginate";
 import LoadingSpinner from "./ui/LoadingSpinner";
+import CharLists from "./ui/characterslists-components/CharLists";
 import { useState } from "react";
 import { useCharacters } from "../hooks/useCharacters";
-
-type Characters = {
-  id: string;
-  gender: string;
-  species: string;
-  name: string;
-  image: string;
-  status: string;
-  origin: {
-    name: string;
-    dimension: string;
-  };
-};
 
 const CharactersLists = (): JSX.Element => {
   const [pages, setPages] = useState<number>(1);
@@ -34,53 +20,7 @@ const CharactersLists = (): JSX.Element => {
   return (
     <section>
       <div className={classes["items-container"]}>
-        <ul>
-          {data?.characters.results.map(
-            (char: Characters) => (
-              <Link key={char.id} href={`/${char.id}`}>
-                <a>
-                  <li
-                    className={classes["items-wrapper"]}
-                    key={char.id}
-                  >
-                    <Image
-                      src={char.image}
-                      width={400}
-                      height={400}
-                      priority
-                    />
-                    <div className={classes["items-info"]}>
-                      <h2>{char.name}</h2>
-                      <div>
-                        <span
-                          className={
-                            char.status === "Dead"
-                              ? `${classes.status} ${classes.dead}`
-                              : classes.status
-                          }
-                        />
-                        {char.status} - {char.species}
-                      </div>
-                      <div className={classes.origin}>
-                        <h4>Origin location:</h4>
-                        <p>{char.origin.name}</p>
-                      </div>
-                      <div className={classes.origin}>
-                        <h4>Origin dimension:</h4>
-                        {char.origin.dimension === null ||
-                        char.origin.dimension === "" ? (
-                          <p>unknown</p>
-                        ) : (
-                          <p>{char.origin.dimension}</p>
-                        )}
-                      </div>
-                    </div>
-                  </li>
-                </a>
-              </Link>
-            )
-          )}
-        </ul>
+        <CharLists data={data?.characters} />
         <Paginate
           info={data?.characters.info.pages}
           page={pages}
---
+++ components/ui/characterslists-components/CharLists.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import Link from "next/link";
+import classes from "./CharLists.module.css";
+
+type Characters = {
+  id: string;
+  gender: string;
+  species: string;
+  name: string;
+  image: string;
+  status: string;
+  origin: {
+    name: string;
+    dimension: string;
+  };
+};
+
+type Data = {
+  data: any;
+};
+
+const CharLists = ({ data }: Data): JSX.Element => {
+  return (
+    <ul className={classes["char-list"]}>
+      {data.results.map((char: Characters) => (
+        <Link key={char.id} href={`/${char.id}`}>
+          <a>
+            <li
+              className={classes["items-wrapper"]}
+              key={char.id}
+            >
+              <Image
+                src={char.image}
+                width={400}
+                height={400}
+                priority
+              />
+              <div className={classes["items-info"]}>
+                <h2>{char.name}</h2>
+                <div>
+                  <span
+                    className={
+                      char.status === "Dead"
+                        ? `${classes.status} ${classes.dead}`
+                        : classes.status
+                    }
+                  />
+                  {char.status} - {char.species}
+                </div>
+                <div className={classes.origin}>
+                  <h4>Origin location:</h4>
+                  <p>{char.origin.name}</p>
+                </div>
+                <div className={classes.origin}>
+                  <h4>Origin dimension:</h4>
+                  {char.origin.dimension === null ||
+                  char.origin.dimension === "" ? (
+                    <p>unknown</p>
+                  ) : (
+                    <p>{char.origin.dimension}</p>
+                  )}
+                </div>
+              </div>
+            </li>
+          </a>
+        </Link>
+      ))}
+    </ul>
+  );
+};
+
+export default CharLists;
